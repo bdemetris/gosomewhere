73a6d050a7a696d8cae4ea8fbc4eba5f82bec791
Brett Demetris <brett.demetris@airbnb.com>
2017-03-17 19:45:03 -0700

    added structure to application

---
+++ weatherdata/query.go
@@ -0,0 +1,35 @@
+package weatherdata
+
+import (
+  "encoding/json"
+  "net/http"
+  "fmt"
+)
+
+func Query(city string) (weatherData, error) {
+	apikey := LoadConfig("./config.json")
+
+	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?APPID=%s&q=%s", apikey.Key, city)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var d weatherData
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+
+	return d, nil
+}
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
